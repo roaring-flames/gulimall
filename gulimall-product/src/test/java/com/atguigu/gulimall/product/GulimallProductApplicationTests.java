d9a6cdd2542329ced65c1a57fe12709043125782
roaring-flames <jinliansheng@163.com>
2020-04-13 22:34:17 +0800

    添加测试用例

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,16 +1,31 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
+
 
     @Test
     public void contextLoads() {
+        BrandEntity brand = new BrandEntity();
+       /* brand.setName("huawei");
+        brandService.save(brand);
+        System.out.println("保存成功！");*/
+
+        brand.setBrandId(1l);
+        brand.setDescript("测试");
+        brandService.updateById(brand);
+
     }
 
 }
